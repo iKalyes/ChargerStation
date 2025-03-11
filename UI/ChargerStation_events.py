a20d26aa8e2a7935edf65e2bb4548c4fb93894b3
iKalyes <go-9@qq.com>
2025-03-11 21:42:48 +0800

    UI UPDATE

---
+++ UI/ChargerStation_events.py
@@ -0,0 +1,32 @@
+
+def FanSwitchON(event_struct):
+    return
+
+
+def FanSwitchOFF(event_struct):
+    return
+
+
+def PwControlON(event_struct):
+    return
+
+
+def PwControlOFF(event_struct):
+    return
+
+
+def USBCPowerON(event_struct):
+    return
+
+
+def USBCPowerOFF(event_struct):
+    return
+
+
+def USBAPowerON(event_struct):
+    return
+
+
+def USBAPowerOFF(event_struct):
+    return
+
